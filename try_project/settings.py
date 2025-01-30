fa0505eacacf0f531f519f702aa89ef4e9080ae5
Bro7777 <oguzhesenli94@gmail.com>
2025-01-30 23:22:13 +0400

    Settings  güncellendi

--- try_project/settings.py
+++ try_project/settings.py
@@ -69,8 +69,8 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'try_project.wsgi.application'
-ASGI_APPLICATION = 'try_project.asgi.application'
+WSGI_APPLICATION = 'try_project.wsgi.application'
+#ASGI_APPLICATION = 'try_project.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
